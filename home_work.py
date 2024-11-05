7ca94ad3f50e17488b8020fae6b3f7b3588f0445
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2024-11-05 15:19:50 +0100

    solved hw-06

--- home_work.py
+++ home_work.py
@@ -11,27 +11,26 @@ class Field:
 
 class Name(Field):
     def __init__(self, value):
-        self.value = value
+        super().__init__(value)
 
 
 class PhoneVerificationError(Exception):
     pass
 
 
-def verify_phone(value: str):
-    if type(value) is not str:
-        raise PhoneVerificationError(f"Incorrect phone data type given: {type(value)}, must be 'str'")
-    if len(value) != 10:
-        raise PhoneVerificationError("Incorrect length of phone, must be 10 digits")
-    for char in value:
-        if not char.isdigit():
-            raise PhoneVerificationError(f"Phone must include only numbers, '{char}' is not a number")
-
-
 class Phone(Field):
     def __init__(self, value: str):
-        verify_phone(value)
-        self.value = value
+        self.verify_phone(value=value)
+        super().__init__(value)
+
+    @staticmethod
+    def verify_phone(value: str):
+        if type(value) is not str:
+            raise PhoneVerificationError(f"Incorrect phone data type given: {type(value)}, must be 'str'")
+        if len(value) != 10:
+            raise PhoneVerificationError("Incorrect length of phone, must be 10 digits")
+        if not value.isdigit():
+            raise PhoneVerificationError(f"Phone must include only numbers, '{value}' is not a number")
 
 
 class Record:
@@ -42,29 +41,33 @@ class Record:
     def add_phone(self, phone: str) -> None | str:
         for phone_record in self.phones:
             if phone_record.value == phone:
-                print("Phone already in the record!")
-                return
+                return "Phone already in the record!"
+
         self.phones.append(Phone(phone))
 
-    def remove_phone(self, phone: str) -> None:
+    def remove_phone(self, phone: str) -> None | str:
         for phone_record in self.phones:
             if phone_record.value == phone:
                 self.phones.remove(phone_record)
         else:
-            print(
-                "Phone you are trying to remove is not in the record. Add the phone"
-            )
+            return "Phone you are trying to remove is not in the record. Add the phone"
 
     def edit_phone(self, old_phone: str, new_phone: str) -> None:
-        for phone_record in self.phones:
-            if phone_record.value == old_phone:
-                phone_record.value = new_phone
-                return
-        else:
+        try:
+            for phone_record in self.phones:
+                if phone_record.value == old_phone and Phone.verify_phone(new_phone):
+                    phone_record.value = new_phone
+                    return
+            else:
+                raise ValueError(
+                    "Incorrect input. Phone you are trying to edit "
+                    "is not in the record or incorrect input type given, "
+                    "must be 'str'."
+                )
+        except PhoneVerificationError:
             raise ValueError(
-                "Incorrect input. Phone you are trying to edit "
-                "is not in the record or incorrect input type given, "
-                "must be 'str'."
+                "New phone value is incorrect. "
+                "Must be 10 digits long, include only letters and be of 'str' type"
             )
 
     def find_phone(self, phone: str):
